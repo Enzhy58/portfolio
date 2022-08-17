1f17e5f0d843566ba667d11e1beb10a1ceeab868
Enzhy58 <ang.borisova2015@yandex.ru>
2022-08-17 20:03:17 +0300

    added vars and added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,48 @@
 $(function(){
+  $('.skills__list').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    slidesToShow: 7,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/next.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1120,
+        settings: {
+          slidesToShow: 6,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 5,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 568,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
+  });
 
+  $('.header__link').on('click', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'), top = $(id).offset().top;
+    $('body, html').animate({ scrollTop: top }, 1500);
+  });
 });
